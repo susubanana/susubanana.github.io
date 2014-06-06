349bf8a4462ec2717459dd580cd9dafe292912b3
susubanana <864181771@qq.com>
2014-06-06 10:53:16 +0800

    Site updated: 2014-06-06 10:53:15

--- js/flappy_js/bird.js
+++ js/flappy_js/bird.js
@@ -22,7 +22,7 @@ FlappyBird.bird = (function(){
 
         //制作一个拥有飞行动作的鸟
         var spriteSheet = new createjs.SpriteSheet({
-            images: ["images/bird.png"],
+            images: ["images/flappy_images/bird.png"],
             frames: {width:42, height:42},
             animations: {
                 //通过背景图片0-2个小鸟实现小鸟的飞行动作
