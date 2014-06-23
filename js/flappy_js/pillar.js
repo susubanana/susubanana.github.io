b540c8aa8f59a3b3db489ecf7d3279700073b405
susubanana <864181771@qq.com>
2014-06-23 18:51:45 +0800

    Site updated: 2014-06-23 18:51:45

--- js/flappy_js/pillar.js
+++ js/flappy_js/pillar.js
@@ -5,7 +5,7 @@ FlappyBird.pillar = (function () {
     var bird = new FlappyBird.bird();
     var playGame = null;
     var vPillar = 4.5;
-    var gap;
+    var gap = 140;
     var Pillar = function(){
         this.pass = false;
         this.pillarWidth = 74;
@@ -18,7 +18,6 @@ FlappyBird.pillar = (function () {
         var stage = stage;
         var pillar1 = new createjs.Bitmap("images/flappy_images/pipe.png");
         var pillar2 = new createjs.Bitmap("images/flappy_images/pipe.png");
-        gap = stage.height * 0.28;
         pillar1.x = x + 30;
 
         //通过随机数实现管道的随机缝隙
